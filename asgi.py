a8b66efdaf1eb8bc50f75fed172774374d5b612d
Kenneth017 <kennethcharles.sarmiento@gmail.com>
2025-01-21 21:52:58 +0800

    Update asgi.py

--- asgi.py
+++ asgi.py
@@ -1,13 +1,4 @@
-import os
 from app import app
 
-if __name__ == "__main__":
-    import uvicorn
-    port = int(os.environ.get("PORT", 10000))
-    uvicorn.run(
-        "app:app",
-        host="0.0.0.0",
-        port=port,
-        log_level="info",
-        workers=4
-    )
+# This is for ASGI servers like Uvicorn
+application = app
