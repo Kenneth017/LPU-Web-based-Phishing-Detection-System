1e2e80a871117b9930f23914dd39457acf8160f9
Kenneth017 <kennethcharles.sarmiento@gmail.com>
2025-01-21 22:12:36 +0800

    Delete asgi.py

--- asgi.py
+++
@@ -1,7 +0,0 @@
-import os
-from app import app
-
-if __name__ == "__main__":
-    import uvicorn
-    port = int(os.environ.get("PORT", 10000))
-    uvicorn.run("asgi:app", host="0.0.0.0", port=port, log_level="info")
