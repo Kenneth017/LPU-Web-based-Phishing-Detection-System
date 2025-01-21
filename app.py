23feab891b1ce3ed511792f2eba3188816dd6caa
Kenneth017 <kennethcharles.sarmiento@gmail.com>
2025-01-22 00:23:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2035,5 +2035,5 @@ migrate_database()  # Then migrate if needed
 feedback_handler = FeedbackHandler()
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
