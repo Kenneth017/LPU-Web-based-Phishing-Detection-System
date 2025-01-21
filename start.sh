011d162252df4e4811af6a5210bcc16cba322cb2
Kenneth017 <kennethcharles.sarmiento@gmail.com>
2025-01-21 20:39:44 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
-#!/bin/bash
+echo '#!/bin/bash
 export PORT="${PORT:-10000}"
-hypercorn app:app --bind "0.0.0.0:$PORT" --access-logfile - --error-logfile -
+hypercorn app:app --bind "0.0.0.0:$PORT" --access-logfile - --error-logfile -' > start.sh
+
+# Add and commit the files
+git add start.sh render.yaml
+git commit -m "Add start script and Render configuration"
+git push
