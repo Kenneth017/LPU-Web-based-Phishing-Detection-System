fc769ea2a1483658379a48af7844ebede9e55b81
Kenneth017 <kennethcharles.sarmiento@gmail.com>
2025-01-21 22:20:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2033,3 +2033,6 @@ ensure_db_directory()
 migrate_database()
 feedback_handler = FeedbackHandler()
 
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
