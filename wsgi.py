925b3686c9cc93c3427005e6b6515e9d9117b241
Kenneth017 <kennethcharles.sarmiento@gmail.com>
2025-01-21 19:09:40 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,7 @@
-from app import app as application
+from app import app
+import asyncio
+from hypercorn.config import Config
+from hypercorn.asyncio import serve
 from utils import setup_logger
 import os
 from dotenv import load_dotenv
@@ -9,6 +12,10 @@ load_dotenv()
 # Set up logging
 logger = setup_logger(__name__)
 
+def run_app():
+    config = Config()
+    config.bind = [f"0.0.0.0:{int(os.environ.get('PORT', 10000))}"]
+    asyncio.run(serve(app, config))
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    application.run(host='0.0.0.0', port=port)
+    run_app()
