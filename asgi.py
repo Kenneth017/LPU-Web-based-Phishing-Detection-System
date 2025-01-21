442358ef0246f08d288d11c40c97734f4479d8c9
Kenneth017 <kennethcharles.sarmiento@gmail.com>
2025-01-21 21:18:18 +0800

    Create asgi.py

---
+++ asgi.py
@@ -0,0 +1,13 @@
+import os
+from app import app
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run(
+        "app:app",
+        host="0.0.0.0",
+        port=port,
+        log_level="info",
+        workers=4
+    )
