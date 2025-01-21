6529da04a8ebe957a77028757e976cdd893be2c7
Kenneth017 <kennethcharles.sarmiento@gmail.com>
2025-01-21 22:41:16 +0800

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Server settings
-HOST = os.getenv('HOST', '0.0.0.0')
-PORT = int(os.getenv('PORT', 10000))
-WORKERS = int(os.getenv('WEB_CONCURRENCY', 4))
-
-# App settings
-DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
