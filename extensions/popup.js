91f80a0370cce71fd840c387c0241dfb8adf035a
Kenneth017 <kennethcharles.sarmiento@gmail.com>
2025-03-01 18:27:26 +0800

    Delete extensions directory

--- extensions/popup.js
+++
@@ -1,8 +0,0 @@
-// popup.js
-document.getElementById('checkUrlButton').addEventListener('click', function() {
-    const url = document.getElementById('urlInput').value;
-    chrome.runtime.sendMessage({
-        action: "userInitiatedCheck",
-        url: url
-    });
-});
